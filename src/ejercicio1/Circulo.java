63ddeea0569ae3e0e897254a330e2c9efb722b86
juanes <jromerop7@unicartagena.edu.com>
2025-10-23 10:19:50 -0500

    primer commit

---
+++ src/ejercicio1/Circulo.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+public class Circulo extends figura {
+private double radio;
+    protected Circulo(double radio,double x, double y) {
+        super(x, y);
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        double pi = 3.14;
+        double resultado = pi * radio * radio;
+        return resultado;
+}
+}
